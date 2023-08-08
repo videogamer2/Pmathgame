4f310a3ba29c5ad5dad5746ee5984f3164f5a730
videogamer2 <116688075+videogamer2@users.noreply.github.com>
2023-08-08 15:26:42 -0300

    yes
    insanity (jk lmaooo)
    geometry dashsda

--- main.js
+++ main.js
@@ -1,1 +1,8 @@
 //não dá certo????
+function adduser(){
+    var nameOne = document.getElementById("J1").value;
+    var nameTwo = document.getElementById("J2").value;
+    localStorage.setItem("NameOne", nameOne);
+    localStorage.setItem("NameTwo", nameTwo);
+    window.location="gamepage.html";
+}
