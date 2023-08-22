88f7a65fc18a25c8fbc3e280a0163d2de994441a
videogamer2 <116688075+videogamer2@users.noreply.github.com>
2023-08-22 16:43:00 -0300

    idk what to name these anymore
    trueffet moment amirite
    
    made the McThing work
    
    trueffet at number 26 on the demone list as of writing this (22/08/¹2023)
    
    mfw
    mayhem

--- gameplay.js
+++ gameplay.js
@@ -1,17 +1,29 @@
-var playerOneName = localStorage.getItem(NameOne);
+var playerOneName = localStorage.getItem("NameOne");
 console.log(playerOneName);
-var playerTwoName = localStorage.getItem(NameTwo);
+var playerTwoName = localStorage.getItem("NameTwo");
 console.log(playerTwoName);
-var aturnChecker = var playerTwoName = localStorage.getItem(NameTwo);
+var aturnChecker = playerTwoName = localStorage.getItem("NameTwo");
 console.log(playerTwoName);
+var points1 = 0;
+var points2 = 0;
 //change this once they get swapped too
 //this is what coding at 6AM does to you
 
 function playerset(){
     document.getElementById("qturn").innerHTML="Turno de Pergunta: " + playerOneName;
     document.getElementById("aturn").innerHTML="Turno de Resposta: " + playerTwoName;
+    document.getElementById("name1").innerHTML = playerOneName;
+    document.getElementById("score1").innerHTML = points1;
+    document.getElementById("name2").innerHTML = playerTwoName;
+    document.getElementById("score2").innerHTML = points2;
 }
 function startgame(){
-    document.getElementById("output").innerHTML="<h3 id='turnMan'></h3> <br> <h3 id='placeholder'></h3>";
+    document.getElementById("output").innerHTML="<h3 id='turnMan'></h3> <br> <h3 id='placeholder'></h3> <br> <input type='text' id='gameInput'></input> <br> <button onclick='answerProcedure' id='sender'>responder</button>";
     document.getElementById("turnMan").innerHTML="Sua vez, " + aturnChecker + "!";
+    document.getElementById("number1").innerHTML=" ";
+    document.getElementById("number2").innerHTML=" ";
+     var num1 = document.getElementById("number1").value;
+     var num2 = document.getElementById("number2").value;
+     var ans = parseInt(num1) * parseInt(num2);
+     document.getElementById("placeholder").innerHTML=num1+"x"+num2;
 }
